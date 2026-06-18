b54efd6368253689c008a6832f2c68cc09554a12
Leo Barbosa <leobarbosapinturas@gmail.com>
2026-06-18 20:37:19 -0300

    chore(qrcode): update qrcode target url with token
    regenerate the visualizar.png qr code image to match the updated url

--- qrcode/qrcode-site.py
+++ qrcode/qrcode-site.py
@@ -2,7 +2,7 @@ import qrcode
 from PIL import Image   
 
 # O conteúdo que o QR Code vai carregar (pode ser link, texto, piX...)
-dados = "https://www.leob.com.br/visualizar-certificado"
+dados = "https://www.leob.com.br/visualizar-certificado.html?token=lb-2026-001"
 
 # Criando o QR Code
 qr = qrcode.make(dados)
